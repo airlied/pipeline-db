13435bb6149d7453fbc54529c6386c1e0ab73c00
Dave Airlie <airlied@redhat.com>
2016-11-22 04:48:30 +0000

    dump: set VK_LAYER_PATH

--- dump_sascha_demos.sh
+++ dump_sascha_demos.sh
@@ -6,6 +6,7 @@ VKTRACE_DIR=$HOME/devel/vulkan/VulkanTools/vktrace
 DEMO_DIR=$HOME/devel/vulkan/samples/Vulkan/build
 OUTDIR=$HOME/devel/vulkan/pipeline-db/tracefiles/$NAME
 
+export VK_LAYER_PATH=$VKTRACE_DIR/src/vktrace_layer/linux
 mkdir -p $OUTDIR
 cd $VKTRACE_DIR
 _VK_TRACE_PDB_MODE=1 ./vktrace -p $DEMO_DIR/bin/$NAME -w $DEMO_DIR -o $OUTDIR/$NAME.trace
